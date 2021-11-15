aa81ff176c0f1bbd704bae23dc9ba3424ebcf133
sakibssamad <sssakib.info@gmail.com>
2021-11-15 11:06:23 +0600

    Lesson 2: Layouts Multiple Text

--- app/src/main/java/com/scube/composebasics/MainActivity.kt
+++ app/src/main/java/com/scube/composebasics/MainActivity.kt
@@ -6,28 +6,27 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            setContent {
-                MessageCard("Android")
-            }
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Android")
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
-
-
-
