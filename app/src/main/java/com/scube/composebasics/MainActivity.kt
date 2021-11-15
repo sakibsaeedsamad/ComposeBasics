181822cb5d6732067f20605572838f7b3c57eb77
sakibssamad <sssakib.info@gmail.com>
2021-11-15 11:11:03 +0600

    Lesson 2: Layouts Surround with Column

--- app/src/main/java/com/scube/composebasics/MainActivity.kt
+++ app/src/main/java/com/scube/composebasics/MainActivity.kt
@@ -3,6 +3,7 @@ package com.scube.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,14 +20,18 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Text(text = msg.author)
-    Text(text = msg.body)
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-    )
+    Column {
+        MessageCard(
+            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+        )
+    }
 }
